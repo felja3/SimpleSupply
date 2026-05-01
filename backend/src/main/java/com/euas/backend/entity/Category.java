f0928a282c42e336a5278114fec808c88cdc59d1
Feliks <feliks@Felikss-MacBook-Pro.local>
2026-05-01 20:21:47 +0300

    initialization+entity

---
+++ backend/src/main/java/com/euas/backend/entity/Category.java
@@ -0,0 +1,32 @@
+package com.euas.backend.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int category_id;
+
+    @Column(name = "category_name", length = 25, nullable = false, unique = true)
+    private String category_name;
+
+
+    @OneToMany(mappedBy = "category")
+    private List<Item> items;
+
+    public Category() {
+
+    }
+}
---
+++ backend/src/main/java/com/euas/backend/entity/Item.java
@@ -0,0 +1,32 @@
+package com.euas.backend.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+
+
+public class Item {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private int item_id;
+
+    @Column(name = "item_name", length = 25, nullable = false, unique = true)
+    private String item_name;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Item() {
+
+    }
+}
